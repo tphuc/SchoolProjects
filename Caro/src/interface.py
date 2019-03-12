1d25ed2635c118c976d29ee0e34f38158f659033
tphuc <phuccokerml@gmail.com>
2019-03-12 07:51:20 +0700

    delete view after endgame

--- Caro/src/interface.py
+++ Caro/src/interface.py
@@ -45,28 +45,29 @@ class TickBox(Label):
             window = Toplevel(None, width=500, height=500)
             window.wm_title("End Game!")
             Label(window, text="Congratulation!").pack(padx=30, pady=30)
-            
+            self.board.delete()
+
+
 class Menu:
     def __init__(self, root):
         self.canvas = Frame(root, width=WIDTH, height=HEIGHT)
         self.title = Label(self.canvas, text="Caro game", font=("Impact",50)).pack()
 
         options = ["PvP", "PvC", "LoadGame", "Quit"]
-        self._create_button(text=options[0], command=self.pvp)
-        self._create_button(text=options[1], command=self.pvc)
-        self._create_button(text=options[2], command=self.load_game)
-        self._create_button(text=options[3], command=quit)
+
+        self.create_button(text=options[0], command=self.pvp)
+        self.create_button(text=options[1], command=self.pvc)
+        self.create_button(text=options[2], command=self.load_game)
+        self.create_button(text=options[3], command=quit)
+
         self.canvas.pack(padx=10, pady=10)
         Frame(master=self.canvas).pack()
         
-    def _create_button(self, text="Not set", x=0, y=0, width=BTN.width, height=BTN.height, command=None):
+    def create_button(self, text="Not set", x=0, y=0, width=BTN.width, height=BTN.height, command=None):
         btn = Button(self.canvas, width=width,height=height, text=text,font="Impact 20",command=command).pack()
         return btn
 
-    def _create_board_game(self, title="window"):
-        window = BoardWidget(self._create_window(title))
-
-    def _create_popup(self, master, title=""):
+    def create_popup(self, master, title=""):
         window = Toplevel(self.canvas)
         window.wm_title(title)
         return window
@@ -78,7 +79,8 @@ class Menu:
             player2 = Player(name_entry2.get())
             pop_window.destroy()
             game = Game(player1, player2, Board())
-        pop_window = self._create_popup(self.canvas)
+    
+        pop_window = self.create_popup(self.canvas)
         Label(pop_window, text="Player1's name:", font="Impact 20").pack()
         name_entry1 = Entry(pop_window, width=10)
         name_entry1.pack()
@@ -87,10 +89,14 @@ class Menu:
         name_entry2.pack()
         Button(pop_window, text="Start", command=start).pack()
 
+
     def pvc(self):
         pass
 
     def load_game(self):
+        pass
+
+    def save_game(self):
         pass
 
 
@@ -99,6 +105,7 @@ class Game():
         self.player1 = player1
         self.player2 = player2
         self.board = board
+
 
 class Player():
     def __init__(self, name="player", label=None):
@@ -137,7 +144,7 @@ class Board:
     
     def check_endgame(self):
         possible_directs = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (-1, 1), (1, -1)] # Unit vectors
-    
+
         def normalize(vec):
             dy, dx = vec
             if dy:
@@ -145,7 +152,7 @@ class Board:
             if vec[1]:
                 dx /= abs(dx)
             return dy, dx
-    
+
         def check_endgame(marked_ls):
             if marked_ls:
                 (y1, x1) = marked_ls[0]
@@ -167,16 +174,11 @@ class Board:
         if check_endgame(self.list_markedO) or check_endgame(self.list_markedX):
             return True
 
-
-                
-                    
-
-
-        
-
-
-
-
+    def delete(self):
+        for ls in self.board:
+            for tickbox in ls:
+                ls.destroy()
+        self.status.destroy()
 
 
 menu = Menu(root)
